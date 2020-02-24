0c3f33dda6ad41f8c90fef307b37a1f0c2ddfc84
DDevSyn <61219024+DDevSyn@users.noreply.github.com>
2020-02-24 23:21:45 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const {Client, Attachment} = require('discord.js');
-const {RichEmbed} = require('discord.js');
+const {Client, RichEmbed} = require('discord.js');
 const bot = new Client();
 
 const PREFIX = '!';
